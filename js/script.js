abf6f32553a9d9a09c8fbcb5254486860eaf3d01
Hans Choi <personabe1984@gmail.com>
2018-04-25 18:26:31 -0400

    feat: Adding a oool carousel feature to show my skill levels in a pie chart

--- js/script.js
+++ js/script.js
@@ -14,4 +14,26 @@ $(document).ready(function() {
     showCursor: false
   });
 
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  });
+
+  $('.chart').easyPieChart({
+            //your options goes here
+
+  });
+  
 });
