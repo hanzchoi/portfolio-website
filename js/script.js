641b2bdbea36776340d057625eae7957c46fb50f
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-15 18:51:52 -0400

    feat: Implemented the countUp to my stat elements

--- js/script.js
+++ js/script.js
@@ -54,4 +54,12 @@ $(document).ready(function() {
     }
   });
 
+
+  $(".counter").each(function(){
+    var element = $(this);
+    var endVal = parseInt(element.text());
+
+    element.countup(endVal);
+  })
+
 });
